57d933d35da21962f85139f65645e81aa34bea14
Louis Martin <louimart@gmail.com>
2024-02-05 17:29:55 -0600

    feedbackApi added for axios calls

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -5,7 +5,19 @@ const pool = require('../modules/pool')
 
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
-
+  const newFeedback = req.body;
+  const sqlText = `INSERT INTO feedback (feeling, understanding, support, comments)
+                    VALUES ($1, $2, $3, $4)`
+  pool
+    .query(sqlText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+    .then ((result) => {
+      console.log('POST successfully added new feedback')
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`ERROR making database query POST: ${sqlText}`, error);
+      res.sendStatus(500);
+    })
 })
 
 
